70c46204eda2085ea2513f31a2776ca7ec135666
Victoriabunmi72 <bhummhie97@gmail.com>
2024-12-18 02:13:23 +0100

    Element script.js

---
+++ Alpha/script.js
@@ -0,0 +1,7 @@
+// Select elements
+
+const images = document.querySelectorAll(".gallery img");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+let currentIndex = 0;
