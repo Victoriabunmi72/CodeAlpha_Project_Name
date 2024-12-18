a38adf030d5cb0f6d39a1bce43819c492dd81e90
Victoriabunmi72 <bhummhie97@gmail.com>
2024-12-18 02:20:38 +0100

    The functions

--- Alpha/script.js
+++ Alpha/script.js
@@ -5,3 +5,15 @@ const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 
 let currentIndex = 0;
+
+// Function to show the image based on the current index
+
+function showImage(index) {
+    // Remove active class from all images
+    images.forEach((img) => img.classList.remove("active"));
+    // Add active to the current image
+    images[index].classList.add("active");
+
+    // Show the initial image
+    showImage(currentIndex);
+}
